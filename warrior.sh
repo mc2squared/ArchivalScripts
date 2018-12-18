f73fb7ae877eceff38241d2c1a06c4dc5838d6bd
MC2 <34783624+mc2squared@users.noreply.github.com>
2018-12-17 19:07:52 -0500

    ArchiveTeam Warrior deployment script (Warrior.sh)

---
+++ warrior.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# Warrior.sh - An easy way to deploy ArchiveTeam Warrior instances quickly on fresh Debian installs using Docker
+#
+# Variables
+nick=$1
+proj=auto
+instances=$2
+sudo apt-get update
+sudo apt-get install -y \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/debian \
+   $(lsb_release -cs) \
+   stable"
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y docker-ce
+for i in $(seq $2); do
+	sudo docker run \
+    	  --detach \
+    	  --env DOWNLOADER="$nick" \
+    	  --env CONCURRENT_ITEMS=5 \
+    	  --env SELECTED_PROJECT="$proj" \
+    	  --restart always \
+    	  --name ATwarrior \
+    	  archiveteam/warrior-dockerfile
+done
+echo "Done! $instances containers running ArchiveTeam Warrior are deployed!"
+exit 0
